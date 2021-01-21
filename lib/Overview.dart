3057fca46ef73d029f300e91cd91f5fa06d6b480
Franc Balakim <balakress@gmail.com>
2021-01-21 11:43:22 +0000

    weekly report

--- lib/Overview.dart
+++ lib/Overview.dart
@@ -1,31 +1,94 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
 
 class Overview extends StatelessWidget {
   Widget build(BuildContext context) {
-    return (Column(children: <Widget>[
-      Container(
-        margin: EdgeInsets.symmetric(vertical: 20.0),
-        height: 38.0,
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 5),
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            FoodsLine(),
-            FoodsLine(),
-            FoodsLine(),
-            FoodsLine(),
-            FoodsLine(),
-          ],
-        ),
-      ),
-      Container(
-        margin: EdgeInsets.symmetric(horizontal: 13),
-        height: 165,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: Colors.white),
-      )
-    ]));
+    return (Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 20.0),
+            height: 38.0,
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                FoodsLine(),
+                FoodsLine(),
+                FoodsLine(),
+                FoodsLine(),
+                FoodsLine(),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 13),
+            padding: EdgeInsets.symmetric(vertical: 10),
+            alignment: Alignment.topLeft,
+            height: 165,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        child: Icon(
+                          Icons.camera_alt,
+                          size: 20,
+                        ),
+                        padding: EdgeInsets.all(5),
+                        shape: CircleBorder(),
+                      ),
+                    ),
+                    Text(
+                      "Weekly report",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                    width: 300,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25),
+                      child: Text(
+                        "You have no upcoming events",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    )),
+                SizedBox(
+                  height: 14,
+                ),
+                Container(
+                    width: 300,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 25),
+                      child: Text(
+                        "Enjoy your week",
+                      ),
+                    ))
+              ],
+            ),
+            // child: SvgPicture.asset("assets/coworkers.svg"),
+            decoration: BoxDecoration(
+                // image: DecorationImage(
+                //   // image: ,
+                //   fit: BoxFit.fill,
+                // ),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white),
+          )
+        ]));
   }
 }
 
